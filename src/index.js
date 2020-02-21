3c4e55a15ff93ddd93d3f6d646739fb016e9c398
Jaroslaw Filipiak <info@j-filipiak.pl>
2020-02-21 06:39:26 +0100

    accordion mobile

--- src/index.js
+++ src/index.js
@@ -9,29 +9,48 @@ const $ = require('jquery');
 //
 // =======================================
 
-var title = $('.our-services--item h3'),
-  accordonContent = $('.excerpt');
+if ($(window).width() < 960) {
+  accordion();
+} else {
 
-accordonContent.hide();
+}
 
-title.click(function() {
-  var excerpt = $(this).next('.excerpt'),
-    isVisible = excerpt.is(':visible');
-
-  title.removeClass('accordion__active');
-  accordonContent.stop().slideUp(300);
-
-  $(this).toggleClass('accordion__active', !isVisible);
-
-  if (!isVisible) {
-    $(this)
-      .next(excerpt)
-      .stop()
-      .slideDown(300);
+$(window).resize(function () {
+  if ($(window).width() < 960) {
+    accordion();
   } else {
-    $(this)
-      .next(excerpt)
-      .stop()
-      .slideUp(300);
+
   }
 });
+
+
+function accordion() {
+
+
+  var title = $('.our-services--item h3'),
+    accordonContent = $('.excerpt');
+
+  accordonContent.hide();
+
+  title.click(function () {
+    var excerpt = $(this).next('.excerpt'),
+      isVisible = excerpt.is(':visible');
+
+    title.removeClass('accordion__active');
+    accordonContent.stop().slideUp(300);
+
+    $(this).toggleClass('accordion__active', !isVisible);
+
+    if (!isVisible) {
+      $(this)
+        .next(excerpt)
+        .stop()
+        .slideDown(300);
+    } else {
+      $(this)
+        .next(excerpt)
+        .stop()
+        .slideUp(300);
+    }
+  });
+}
